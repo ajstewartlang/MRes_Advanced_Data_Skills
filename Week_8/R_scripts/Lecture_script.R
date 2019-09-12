0a8b34df1dec3b8ec125224e2f3a668d5e0bb6a9
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-09-12 13:18:34 +0100

    add week_8

--- Week_8/R_scripts/Lecture_script.R
+++ Week_8/R_scripts/Lecture_script.R
@@ -2,183 +2,107 @@
 
 library(tidyverse) #load the tidyverse packages
 library(afex) #load afex for running factorial ANOVA
-library(DescTools) #load DescTools for calculating effect sizes
 library(emmeans) #load emmeans for running pairwise comparisons
 
-#ANOVA - one factor, three levels between participants design
-#the following code creates our data
-set.seed(1234)
-x1 <- rnorm(n = 15, mean = 5.3, sd = .4)
-x2 <- rnorm(n = 15, mean = 7.1, sd = .45)
-x3 <- rnorm(n = 15, mean = 9.2, sd = .5)
-x4 <- (1:45)
-x5 <- c (x1, x2, x3)
-x6 <- c(rep("Water", 15), rep("Single Espresson", 15), rep("Double Espresso", 15))
+# ANOVA - one factor, three levels between participants design ####
 
-colnames (condc) <- c("Participant", "Condition", "Ability")
+cond <- read_csv("data_files/cond.csv")
 
-cond <- as.data.frame(cond)
 cond$Condition <- as.factor(cond$Condition)
 
-cond$Condition <- relevel(cond$Condition, ref=3)
+cond %>% 
+  ggplot(aes(x = Condition, y = Ability, colour = Condition)) +
+  geom_violin() +
+  geom_jitter(width = .1) +
+  guides(colour = FALSE) +
+  stat_summary(fun.data = "mean_cl_boot", colour = "black") +
+  theme(text = element_text(size = 15))
 
-str(cond)
+cond %>%
+  group_by(Condition) %>%
+  summarise(mean = mean(Ability), sd = sd(Ability))
 
-head(cond)
+model <- aov_4(Ability ~ Condition + (1 | Participant), data = cond)
 
-pirateplot (formula = Ability ~ Condition, data=cond , theme = 0, # Start from scratch
-            inf.method = "sd", # Use standard deviations to define band limits
-            inf.f.o = .7, # Band opacity
-            inf.f.col = piratepal("basel"), # Add color to bands
-            point.o = 1, # Point opacity
-            avg.line.o = .8, # Average line opacity
-            gl.col = gray(.6), # Gridline specifications
-            gl.lty = 1, # Gridline specifications
-            gl.lwd = c(.5, 0), # Gridline specifications
-            ylim=c(0,10), # Y-axis limits
-            cex.names=.85, # Axis labels scale (1=full scale)
-            main="Data of Ability by Condition") # Graph title
+summary(model)
 
-#generate some descriptives using describeBy
-describeBy(cond$Ability, group = cond$Condition)
+emmeans(model, pairwise ~ Condition)
 
-#generate some descriptives using dplyr
-cond %>% group_by(Condition) %>% summarise(mean = mean(Ability), sd = sd(Ability), count = n())
+# Repeated Measures ANOVA - one factor with 4 levels ####
 
-#run the ANOVA
-model <- aov(Ability ~ Condition, data = cond)
-anova(model)
+rm_data <- read_csv("data_files/rm_data.csv")
+rm_data$Condition <- as.factor(rm_data$Condition)
+rm_data
 
-#running pairwise comparisons with corrections - also get CIs
-emmeans(model, pairwise ~ Condition, adjust = "Bonferroni")
-emmeans(model, pairwise ~ Condition, adjust = "Tukey")
+rm_data %>%
+  group_by(Condition) %>%
+  summarise(mean = mean(Score), sd = sd (Score))
 
-#get effect size of Condition
-EtaSq(model, type = 3, anova = TRUE)
+rm_data %>%
+  ggplot(aes(x = Condition, y = Score, colour = Condition)) +
+  geom_violin() +
+  geom_jitter(width = .1) +
+  guides(colour = FALSE) +
+  stat_summary(fun.data = "mean_cl_boot", colour = "black") +
+  theme(text = element_text(size = 15))
 
+rm_data %>%
+  ggplot(aes(x = fct_reorder(Condition, Score), y = Score, colour = Condition)) +
+  geom_violin() +
+  geom_jitter(width = .1) +
+  guides(colour = FALSE) +
+  stat_summary(fun.data = "mean_cl_boot", colour = "black") +
+  theme(text = element_text(size = 15)) +
+  labs(x = "Condition")
 
-
-#Repeated Measures ANOVA - one factor
-#generate data - repeated measures, one factor with 4 levels 
-set.seed(1234)
-x1 <- rnorm(n = 32, mean = 85, sd = 4)
-x2 <- rnorm(n = 32, mean = 84, sd = 4)
-x3 <- rnorm(n = 32, mean = 72, sd = 7)
-x4 <- rnorm(n = 32, mean = 54, sd = 6)
-x5 <- c((1:32), (1:32), (1:32), (1:32))
-x6 <- c(rep("Very Easy", 32), rep("Easy", 32), rep("Hard", 32), rep("Very Hard", 32))
-
-data <- c(x1, x2, x3, x4)
-data <- cbind (x5, x6, data)
-data <- as.tibble(data)
-
-colnames (data) <- c("Participant", "Condition", "Score")
-
-data$Condition <- factor(data$Condition, levels = c("Very Easy", "Easy", "Hard", "Very Hard"))
-
-data$Score <- as.integer(data$Score)
-
-#Pirateplot
-pirateplot (formula = Score ~ Condition, data=data , theme = 0, # Start from scratch
-            inf.f.o = .7, # Band opacity
-            inf.f.col = piratepal("basel"), # Add color to bands
-            point.o = 1, # Point opacity
-            avg.line.o = .8, # Average line opacity
-            gl.col = gray(.6), # Gridline specifications
-            gl.lty = 1,
-            inf.method = 'sd',
-            gl.lwd = c(.5, 0), ylim=c(0,100))
-
-#plot for each participant
-ggplot(data, aes (Condition, Score, colour = Condition)) + 
-  ylim(0,100) + geom_point() + facet_wrap(~ data$Participant)
-
-#generate some descriptives
-describeBy(data$Score, group = data$Condition)
-
-#build our model and get the output
-model <- aov_4(Score ~ Condition + (1 + Condition | Participant), data = data)
+model <- aov_4(Score ~ Condition + (1 + Condition | Participant), data = rm_data)
 summary(model)
 anova(model)
 
-#running pairwise comparisons with corrections - also get CIs
-emmeans (model, pairwise ~ Condition, adjust = "Bonferroni")
+emmeans(model, pairwise ~ Condition, adjust = "Bonferroni")
 
+# 2 x 2 repeated design long data format ####
 
-#2 x 2 repeated design long data format
-DV <- read_csv("DV.csv", col_types = cols(Context = col_factor(levels = c("Positive", 
-                                                                          "Negative")), 
-                                          Sentence = col_factor(levels = c("Positive", "Negative"))))
+fact_data <- read_csv("data_files/fact_data.csv")
+fact_data$Sentence <- as.factor(fact_data$Sentence)
+fact_data$Context <- as.factor(fact_data$Context)
 
-#Generate descriptives
-describeBy(DV$RT, group = list(DV$Sentence, DV$Context))
+fact_data
 
-#Plotting raw data in pirateplot
-pirateplot (formula = RT ~ Sentence*Context, data=DV , theme = 0, # Start from scratch
-            inf.f.o = .7, # Band opacity
-            inf.f.col = piratepal("basel"), # Add color to bands
-            point.o = 1, # Point opacity
-            avg.line.o = .8, # Average line opacity
-            gl.col = gray(.6), # Gridline specifications
-            gl.lty = 1,
-            inf.method = 'sd',
-            gl.lwd = c(.5, 0))
+fact_data %>%
+  group_by(Context, Sentence) %>%
+  summarise(mean = mean(RT), sd = sd(RT))
 
-#Plotting data aggregated
-data_agg <- DV %>% group_by(Sentence, Context) %>% summarise_at("RT", c(Mean, sd), na.rm = T)
-colnames(data_agg) <- c("Sentence", "Context", "RT", "SD")
-ggplot(data_agg, aes(x = Sentence, y = RT, group = Context, colour = Context)) + geom_point() + geom_line()
+visdat::vis_miss(fact_data)
 
-#Visualising as Raincloud Plots
-library(RColorBrewer)
-library(plyr) #note, need to detach this after this plot as clashes with aspects of dplyr
-source("https://gist.githubusercontent.com/ajstewartlang/6c4cd8ab9e0c27747424acdfb3b4cff6/raw/fb53bd97121f7f9ce947837ef1a4c65a73bffb3f/geom_flat_violin.R")
+fact_data %>%
+  filter(!is.na(RT)) %>%
+  group_by(Context, Sentence) %>%
+  summarise(mean = mean(RT), sd = sd(RT))
 
-raincloud_theme = theme(
-  text = element_text(size = 12),
-  axis.title.x = element_text(size = 12),
-  axis.title.y = element_text(size = 12),
-  axis.text = element_text(size = 12),
-  axis.text.x = element_text(angle = 45, vjust = 0.5),
-  legend.title=element_text(size=12),
-  legend.text=element_text(size=12),
-  legend.position = "right",
-  plot.title = element_text(lineheight=.8, face="bold", size = 16),
-  panel.border = element_blank(),
-  panel.grid.minor = element_blank(),
-  panel.grid.major = element_blank(),
-  axis.line.x = element_line(colour = 'black', size=0.5, linetype='solid'),
-  axis.line.y = element_line(colour = 'black', size=0.5, linetype='solid'))
+fact_data %>%
+  group_by(Context, Sentence) %>%
+  summarise(mean = mean(RT, na.rm = TRUE), sd = sd(RT, na.rm = TRUE))
 
-lb <- function(x) mean(x) - sd(x)
-ub <- function(x) mean(x) + sd(x)
+fact_data %>%
+  ggplot(aes(x = Context:Sentence, y = RT, colour = Context:Sentence)) +
+  geom_violin() +
+  geom_jitter(width = .1, alpha = .25) +
+  guides(colour = FALSE) +
+  stat_summary(fun.data = "mean_cl_boot", colour = "black") +
+  theme(text = element_text(size = 15), axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Condition")
 
-sumld <- ddply(dataRT, ~Condition, summarise, mean = mean(RT), median = median(RT), 
-               lower = lb(RT), upper = ub(RT))
+# By Subjects ANOVA ####
+model_subjects <- aov_4(RT ~ Context * Sentence + (1 + Context * Sentence | Subject), data = fact_data, na.rm = TRUE)
 
-ggplot(data = DV, aes(y = RT, x = Sentence:Context, fill = Sentence:Context)) +
-  geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8, trim=FALSE) +
-  geom_point(aes(y = RT, color = Sentence:Context), position = position_jitter(width = .15), size = .5, alpha = 0.8) +
-  geom_boxplot(width = .1,  outlier.shape = NA, alpha = 0.5) +
-  expand_limits(x = 3) +
-  guides(fill = FALSE) +
-  guides(color = FALSE) +
-  scale_color_brewer(palette = "Accent") +
-  scale_fill_brewer(palette = "Accent") +
-  coord_flip() +
-  theme_bw() +
-  raincloud_theme +
-  labs(x=NULL)  +
-  scale_y_continuous(breaks = seq(0,9000,by = 2000))
+anova(model_subjects)
 
-detach("package:plyr", unload=TRUE) #need to detach as some clashes with dplyr
+emmeans(model_subjects, pairwise ~ Context * Sentence, adjust = "none")
 
-#By Subjects
-model <- aov_4(RT ~ Sentence * Context + (1+Sentence * Context | Subject), data = DV, na.rm = TRUE)
-anova(model)
+# By Items ANOVA ####
+model_items <- aov_4(RT ~ Context * Sentence + (1 + Context * Sentence | Item), data = fact_data, na.rm = TRUE)
 
-#By Items
-model1 <- aov_4(RT ~ Sentence * Context + (1 + Sentence * Context | Item), data = DV, na.rm = TRUE)
-anova(model1)
+anova(model_items)
 
-emmeans(model, pairwise ~ Sentence * Context, adjust = "none")
+emmeans(model_items, pairwise ~ Context * Sentence, adjust = "none")
